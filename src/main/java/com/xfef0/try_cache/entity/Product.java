f1200590e17a4f7d97028fffee871269a5fe9653
Federico Ramayo <52141168+xFEF0@users.noreply.github.com>
2025-02-25 19:01:13 -0300

    use redis cache

--- src/main/java/com/xfef0/try_cache/entity/Product.java
+++ src/main/java/com/xfef0/try_cache/entity/Product.java
@@ -9,11 +9,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @Entity
-public class Product{
+public class Product implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
