1eb58800098b93667ff9c9ed8f76d2be9b14492c
Federico Ramayo <52141168+xFEF0@users.noreply.github.com>
2025-02-25 14:57:58 -0300

    initial commit

---
+++ src/main/java/com/xfef0/try_cache/TryCacheApplication.java
@@ -0,0 +1,13 @@
+package com.xfef0.try_cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryCacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xfef0/try_cache/TryCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xfef0.try_cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
