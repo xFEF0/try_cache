a4101cdf29eefd4a607d234664c96688a2691543
Federico Ramayo <52141168+xFEF0@users.noreply.github.com>
2025-03-05 17:25:00 -0300

    add TTL configuration through bean

---
+++ src/main/java/com/xfef0/try_cache/config/RedisConfig.java
@@ -0,0 +1,22 @@
+package com.xfef0.try_cache.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(20))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(new GenericJackson2JsonRedisSerializer()));
+    }
+}
